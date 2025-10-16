312bfee08294e229c37d3a06eac629f052d964dd
spiritualcoderr@gmail.com <spiritualcoderr@gmail.com>
2025-10-16 20:13:13 +0530

    registration

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,11 @@
 import asynchandler from "../utils/asynchandler.js";
 import {ApiError} from "../utils/ApiError.js"
 import {User} from "../models/user.model.js"
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/ApiResponse.js";
+
+
+
 
 const registerUser = asynchandler( async (req , res )=> {
     const {fullname, email, username, password }  = req.body
@@ -24,7 +29,36 @@ if (
     }
 
 const avatarLocalPath = req.files?.avatar[0]?.path;
+const coverImageLocalPath  = req.files?.coverImage[0]?.path;
 
+if (!avatarLocalPath){
+    throw new ApiError(400, "avatar file is rerquired")
+}
+  
+const avatar = await uploadOnCloudinary(avatarLocalPath)
+const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+if (!avatar){
+    throw new ApiError(400,"Avatar is missing")
+}
+
+const user = await User.create({
+    fullname,
+    avatar : avatar.url,
+    coverImage : coverImage?.url || "",
+    username : username.toLowerCase()
+}) 
+
+const createdUser = await User.findById(user.id).select(
+    "-pasword -refreshToken"
+)
+
+if(!createdUser){
+    throw new ApiError(500, "something went wrong while registering user")
+}
+return res.status(201).json(
+    new ApiResponse(200,createdUser,"user regigster susserfully")
+)
 
 } )
 

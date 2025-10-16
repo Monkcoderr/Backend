50d2ef789c56c817596637530624df8f5c29bf10
spiritualcoderr@gmail.com <spiritualcoderr@gmail.com>
2025-10-16 17:56:19 +0530

    models

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,12 +1,21 @@
 // import { asynchandler } from "../utils/asynchandler.js";
 // user.controller.js
 import asynchandler from "../utils/asynchandler.js";
-
+import {ApiError} from "../utils/ApiError.js"
+import {user} from "../models/index.js"
 
 const registerUser = asynchandler( async (req , res )=> {
     const {fullname, email, username, password }  = req.body
     console.log("email", email);
-  
+
+if (
+    [fullname,email,username,password].some((field)=> field?.trim()==="")
+)
+    {
+        throw new ApiError(400,"all fields are required")
+    }
+
+   
 } )
 
 

07b162fcc773ad5cc9320a3309d5d822bd9c2371
spiritualcoderr@gmail.com <spiritualcoderr@gmail.com>
2025-10-16 18:15:28 +0530

    nea models

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,7 @@
 // user.controller.js
 import asynchandler from "../utils/asynchandler.js";
 import {ApiError} from "../utils/ApiError.js"
-import {user} from "../models/index.js"
+import {User} from "../models/user.model.js"
 
 const registerUser = asynchandler( async (req , res )=> {
     const {fullname, email, username, password }  = req.body
@@ -15,7 +15,13 @@ if (
         throw new ApiError(400,"all fields are required")
     }
 
+    const existedUser = User.findOne({
+        $or: [{username},{email}]
+    })
    
+    if (existed) {
+        throw new ApiError()
+    }
 } )
 
 
